c64578b1d80c7277ed1bfdd17ae8de8ba6153cbc
pawarherschel <pawarherschel@gmail.com>
2024-11-18 16:53:47 +0530

    move stuff

--- src/program.rs
+++ src/program.rs
@@ -7,47 +7,6 @@ use cranelift::prelude::{
 };
 use std::io::{Read, Write};
 
-extern "C" fn write(value: u8) -> *mut std::io::Error {
-    // Writing a non-UTF-8 byte sequence on Windows error out.
-    if cfg!(target_os = "windows") && value >= 128 {
-        return std::ptr::null_mut();
-    }
-    let result = {
-        let mut stdout = std::io::stdout().lock();
-
-        stdout.write_all(&[value]).and_then(|()| stdout.flush())
-    };
-
-    match result {
-        Err(err) => Box::into_raw(Box::new(err)),
-        _ => std::ptr::null_mut(),
-    }
-}
-
-unsafe extern "C" fn read(buf: *mut u8) -> *mut std::io::Error {
-    let mut stdin = std::io::stdin().lock();
-    loop {
-        let mut value = 0;
-        let err = stdin.read_exact(std::slice::from_mut(&mut value));
-
-        if let Err(err) = err {
-            if err.kind() != std::io::ErrorKind::UnexpectedEof {
-                return Box::into_raw(Box::new(err));
-            }
-            value = 0;
-        }
-
-        // ignore CR from Window's CRLF
-        if cfg!(target_os = "windows") && value == b'\r' {
-            continue;
-        }
-
-        *buf = value;
-
-        return std::ptr::null_mut();
-    }
-}
-
 pub const PROGRAM_MEMORY_SIZE: usize = 30_000;
 
 #[derive(Debug, Clone)]
@@ -355,5 +314,46 @@ impl Program {
         let error = unsafe { code_fn(self.memory.as_mut_ptr()) };
 
         assert!(error.is_null(), "{error:#?}");
+    }
+}
+
+extern "C" fn write(value: u8) -> *mut std::io::Error {
+    // Writing a non-UTF-8 byte sequence on Windows error out.
+    if cfg!(target_os = "windows") && value >= 128 {
+        return std::ptr::null_mut();
+    }
+    let result = {
+        let mut stdout = std::io::stdout().lock();
+
+        stdout.write_all(&[value]).and_then(|()| stdout.flush())
+    };
+
+    match result {
+        Err(err) => Box::into_raw(Box::new(err)),
+        _ => std::ptr::null_mut(),
+    }
+}
+
+unsafe extern "C" fn read(buf: *mut u8) -> *mut std::io::Error {
+    let mut stdin = std::io::stdin().lock();
+    loop {
+        let mut value = 0;
+        let err = stdin.read_exact(std::slice::from_mut(&mut value));
+
+        if let Err(err) = err {
+            if err.kind() != std::io::ErrorKind::UnexpectedEof {
+                return Box::into_raw(Box::new(err));
+            }
+            value = 0;
+        }
+
+        // ignore CR from Window's CRLF
+        if cfg!(target_os = "windows") && value == b'\r' {
+            continue;
+        }
+
+        *buf = value;
+
+        return std::ptr::null_mut();
     }
 }
